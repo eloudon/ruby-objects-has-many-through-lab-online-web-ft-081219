143f0ccdcc5bff2b9ae4d521582e6220360c4e39
Eric Loudon <loudon.eric@gmail.com>
2019-08-28 12:51:26 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,33 +1,23 @@
 class Artist
-
-  attr_accessor :name, :artist, :genre
-
-  all = []
+  attr_accessor :name
 
   def initialize(name)
+    @songs = []
     @name = name
-    all << self
-  end
-
-  def self.all
-  all
-  end
-
-  def new_song(name, genre)
-    song = Song.new(name, self, genre)
-    self.songs.last
   end
 
   def songs
-    Song.all.select do |song|
-      song.artist == self
-    end
+    @songs
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
   end
 
   def genres
-    Song.all.map do |song|
+    self.songs.collect do |song|
       song.genre
     end
   end
-
 end
