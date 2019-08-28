145ef036d66173a8b42bc579d1d02c6564a59112
Eric Loudon <loudon.eric@gmail.com>
2019-08-28 13:04:37 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,13 @@
+class Patient
+
+  attr_accessor :name, :appointment, :doctor
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
