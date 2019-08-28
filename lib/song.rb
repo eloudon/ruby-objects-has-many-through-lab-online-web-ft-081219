e08f84e537d46a3bab43ed6fe5c62738a7132b8b
Eric Loudon <loudon.eric@gmail.com>
2019-08-28 12:54:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,16 @@
 class Song
-  attr_accessor :genre, :artist, :name
 
-  def initialize(name, genre)
+  attr_accessor :name, :artist, :genre
+  @@all = []
+
+  def initialize(name, artist, genre)
     @name = name
-    self.genre = genre
-    genre.add_song(self)
+    @artist = artist
+    @genre = genre
+    @@all << self
   end
-end
+
+  def self.all
+    @@all
+  end
+end 
